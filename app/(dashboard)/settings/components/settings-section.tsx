c1b944f2c6ac6eebbbf642d03445d31f6686b055
Jan Poonthong <janpoonthong628@gmail.com>
2025-07-15 15:06:13 +0700

    feat: refactor SettingsSection to use Accordion for better organization and update page title and description

--- app/(dashboard)/settings/components/settings-section.tsx
+++ app/(dashboard)/settings/components/settings-section.tsx
@@ -10,28 +10,81 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer"
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import Image from "next/image"
+import { Separator } from "@radix-ui/react-separator"
+
 export default function SettingsSection() {
   return (
-    <div>
-      <Drawer direction="right">
-        <DrawerTrigger className="hover:underline font-medium cursor-pointer">
+    <Accordion type="single" collapsible className="w-1/2">
+      <AccordionItem value="cloud-providers">
+        <AccordionTrigger className=" cursor-pointer">
           Cloud Providers
-        </DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose className="w-full">
-              <Button className="w-full" variant="outline">
-                Cancel
-              </Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </div>
+        </AccordionTrigger>
+        <AccordionContent className="pl-4">
+          <Drawer direction="right">
+            <DrawerTrigger className="flex items-center justify-center gap-2 hover:underline cursor-pointer">
+              <Image
+                src="/icon/azure.svg"
+                alt="Azure Cloud Provider Icon"
+                width={24}
+                height={24}
+              />
+              Azure
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                <DrawerDescription>
+                  This action cannot be undone.
+                </DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose className="w-full">
+                  <Button className="w-full" variant="outline">
+                    Cancel
+                  </Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+          <br />
+          <Drawer direction="right">
+            <DrawerTrigger className="flex items-center justify-center gap-2 hover:underline cursor-pointer ">
+              <Image
+                src="/icon/aws.svg"
+                alt="AWS Cloud Provider Icon"
+                width={24}
+                height={24}
+              />
+              AWS
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                <DrawerDescription>
+                  This action cannot be undone.
+                </DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose className="w-full">
+                  <Button className="w-full" variant="outline">
+                    Cancel
+                  </Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </AccordionContent>
+      </AccordionItem>
+      <Separator />
+    </Accordion>
   )
 }
--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -4,9 +4,10 @@ export default function Page() {
   return (
     <div className="hidden h-full flex-1 flex-col space-y-8 p-6 md:flex">
       <div className="space-y-2">
-        <h2 className="text-2xl font-bold tracking-tight">Policies</h2>
+        <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
         <p className="text-muted-foreground">
-          Here&apos;s a list of your policies for provisioning infrastructure!
+          Here&apos;s a list of your settings for managing your account and
+          preferences!
         </p>
       </div>
 
