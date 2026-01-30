4ee70c07aa84592887464be583c1f50b8a834bde
HtetAungShine6 <htetaungshine211299@gmail.com>
2026-01-30 15:32:12 +0700

    fix: bugs

--- components/react-scan-component.tsx
+++ components/react-scan-component.tsx
@@ -1,12 +1,13 @@
 "use client"
-import { scan } from "react-scan"
 import { JSX, useEffect } from "react"
 
 export function ReactScan(): JSX.Element {
   useEffect(() => {
     // if (process.env.NODE_ENV !== "production") {
-    scan({
-      enabled: true,
+    import("react-scan").then(({ scan }) => {
+      scan({
+        enabled: true,
+      })
     })
     // }
   }, [])
